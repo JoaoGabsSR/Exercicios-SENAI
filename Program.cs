1001e8106f7899c95445e350b928390ce6f4b74a
João Gabriel <joaogabrielsr39@gmail.com>
2022-04-12 18:58:07 -0300

    Lista de exercícios do curso técnico Desenvolvimento de Jogos Digitais na Instituição SENAI

---
+++ Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace lista_oito_logica_de_programacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int repeat, choose, age;
+            string name;
+
+            do
+            {
+                Console.Write("Entre com qual exercício deseja verificar: "); choose = int.Parse(Console.ReadLine());
+
+                switch(choose) 
+                {
+                    case 1:
+
+                        Console.Write("Entre com seu nome: "); name = Console.ReadLine();
+                        Console.Write("Entre com sua idade: "); age = int.Parse(Console.ReadLine());
+
+                        Mensagem(name, age);
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    default:
+                        break;
+                }
+
+                Console.WriteLine("Deseja fazer algum outro exercício: "+
+                "1 - Sim "+
+                "2 - Não ")
+
+            } while (repeat != 2);
+        }
+
+        // Método para a primeira questão
+        public static void Message(string name, int age)
+        {
+            Console.WriteLine($"Meu nome é {name} e minha idade é {age}");
+        }
+
+        // Método para a segunda questão
+        public static string FullName(string name, string lastName, string secondLastName)
+        {
+            return $"{name} {lastName} {secondLastName}";
+        }
+
+        // Método para a terceira questão
+        public static bool PasswordVerified(int password)
+        {
+            // Senha númerica padrão criada exclusivamente para este exercício
+            if (password == 1234)
+            {
+                return true;
+            } 
+            else 
+            {
+                return false;
+            }
+        }
+
+        // Método para a quarta questão 
+        public static float Media(params int[] values)
+        {
+            // Variáveis do método
+            float media;
+            int sum = 0; 
+
+            // Percorre o vetor e soma os valores
+            for (int i = 0; i < values.Length; i++)
+            {
+                sum += values[i];
+            }
+
+            media = sum / values.Length; // Cálculo para encontrar a média
+
+            return media;
+        }
+
+        // Método para a quinta questão
+        public static int Repetition(int[] values = new int[10], int num)
+        {
+            int count = 0; // Variável do método
+
+            // For para percorrer o vetor e verificar quantas vezes num repete
+            for (int i = 0; i < values.Length; i++)
+            {
+                if (values[i] == num)
+                {
+                    count++; // Acréscimo do count
+                }
+            }
+
+            return count;
+        }
+
+        // Métodos para a sexta questão
+        public static int LePositivos(int num1, int num2, int num3, int num4, int num5)
+        {
+            // Vetores do método
+            int[] numbers = new int[] { num1, num2, num3, num4, num5};
+            int[] sumNums = new int[numbers.Length];
+
+            // For para percorrer os vetores e realizar a somatoria
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sumNums[i] = Somatorio(numbers[i]);
+            }
+
+            return sumNums; // Retorno do método
+        }
+
+        public static int Somatorio(int num)
+        {
+            int sum = 0; // Váriavel do método que será retornado
+
+            // For para realizar a soma dos valores de num e inferiores
+            for(int i = num; i >= 1; i--)
+            {
+                sum += i;
+            }
+
+            return sum; // Retorno do método
+        }
+
+        // Método para a sétima questão
+        public static int 
+    }
+}
+ 
---
+++ Readme.md
@@ -0,0 +1,36 @@
+Lista de exercícios desenvolvidos no conteúdo visto na unidade curricular de Lógica de Programação no curso Técnico de Desenvolvimento de Jogos Digitais ministrados na instituição SENAI Horto, pelo professor Petersen Pereira Dia;
+
+Segue a baixo a lista de exercícios: 
+
+1) Escreva um método que receba como parâmetro o nome e a idade do usuário
+mostre a mensagem, “Meu nome é NOME e tenho IDADE anos”;
+
+2) Escreva um método que receba como parâmetro o nome, sobrenome e o
+último do usuário e mostre na tela seu nome completo;
+
+3) Escreva um método que receba como parâmetro uma senha, que será
+digitada pelo usuário. Caso a senha seja correta o método deve retornar TRUE,
+caso contrário deve retornar FALSE;
+
+4) Escreva um método que receba um vetor de inteiros de tamanho arbitrário (o
+tamanho do vetor e seus elementos deve ser lido pelo programa) e retorne a
+média aritmética de seus valores, mostre o valor da média;
+
+5) Escreva um método que receba um vetor de 10 posições com números
+inteiros e um outro número inteiro. O método deve retornar o total de vezes que
+esse elemento, passado como parâmetro, aparece no vetor;
+
+6) Escreva um programa para ler 5 números inteiros positivos (utilize o método
+LePositivo( )). Para cada valor lido escreva a soma dos inteiros de 1 ao número
+informado. O resultado do cálculo desse somatório deve ser obtido através do
+método Somatorio( );
+
+7) Escreva um método que receba 2 vetores A e B de inteiros com tamanho 10
+por parâmetro. Ao final da execução do método, o vetor B deve conter cada
+elemento de A multiplicado por 3. O vetor B deve ser retornado e mostrado na
+tela;
+
+8) Escreva um método que solicite o ano de nascimento da idade de um
+determinado usuário e retorne sua idade, se for menor que 18 anos exiba a
+mensagem “Sem permissão”, caso seja maior ou igual a 18 anos exiba a
+mensagem “Permissão concedida”;
